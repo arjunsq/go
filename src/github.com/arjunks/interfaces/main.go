7b4604896e5ed26a06aeeda7c186f7292f51f4e6
arjunsq <arjunsoman@qburst.com>
2018-09-03 14:52:06 +0530

    Learnes interfaces

--- src/github.com/arjunks/interfaces/main.go
+++ src/github.com/arjunks/interfaces/main.go
@@ -4,35 +4,78 @@ import (
 	"fmt"
 )
 
+type person struct {
+	fname string
+	lname string
+	age   int
+}
+
+type programmer struct {
+	person
+	lang []string
+}
+
+type agent struct {
+	person
+	licenseToKill bool
+}
+
+type men interface {
+	describe()
+}
+
+func talk(m men) {
+	m.describe()
+}
+
+func (p programmer) describe() {
+	p.person.sayName()
+	fmt.Println("Known languages:", p.lang)
+}
+
+func (a agent) describe() {
+	a.person.sayName()
+	if a.licenseToKill == true {
+		fmt.Println("And has license to kill")
+	} else {
+		fmt.Println("But doesnt have license to kill")
+	}
+}
+
+func (p person) sayName() {
+	fmt.Println("I am", p.lname, ",", p.fname, p.lname)
+}
+
 func main() {
-	var two twoincrementer
-	two = 0
-	tw := &two
-	for i := 0; i < 10; i++ {
-		fmt.Println(tw.increment())
+	bond := person{
+		"James",
+		"Bond",
+		40,
 	}
-
-	var one oneincrementer
-	one = 0
-	on := &one
-	for i := 0; i < 10; i++ {
-		fmt.Println(on.increment())
+	ks := person{
+		"Arjun",
+		"K S",
+		21,
 	}
-}
+	bond.sayName()
+	ks.sayName()
 
-type incrementer interface {
-	increment() int
-}
-
-type twoincrementer int
-type oneincrementer int
-
-func (num *twoincrementer) increment() int {
-	*num = *num + 2
-	return int(*num)
-}
-
-func (num *oneincrementer) increment() int {
-	*num = *num + 1
-	return int(*num)
+	james := agent{
+		person{
+			"James",
+			"Bond",
+			40,
+		},
+		true,
+	}
+	arjun := programmer{
+		person{
+			"Arjun",
+			"K S",
+			21,
+		},
+		[]string{"Java", "C", "Go", "Node.js"},
+	}
+	talk(james)
+	talk(arjun)
 }
